0f3341be019c3433e49d182951d0d4d349b1552b
bsquintana55 <bsquintana55@misena.edu.co>
2022-10-24 11:03:38 -0500

    Portafolio

--- appsnodejs/portafolio/index.js
+++ appsnodejs/portafolio/index.js
@@ -12,23 +12,19 @@ server.use(express.static(__dirname + '/public'));
 server.set('view engine', 'ejs');
 server.set('views',__dirname + '/views');
 
-server.get('/', (req, res)=>{
-    /*res.send("hola brandon escudero")
-    console.log(__dirname)*/
-    res.render('index')
-})
-
+server.use('/Brandon', require('./routes/route')); 
+/*
 server.get('/informacion', (req, res)=>{
     /*res.send("hola brandon escudero")
-    console.log(__dirname)*/
+    console.log(__dirname)
     res.render('informacion')
 })
 
 server.get('/resumen', (req, res)=>{
     /*res.send("hola brandon escudero")
-    console.log(__dirname)*/
+    console.log(__dirname)
     res.render('resumen')
-})
+})*/
 
 server.use((req,res,next)=>{
     res.status(404).sendFile(__dirname + '/public/404.html')
@@ -38,7 +34,7 @@ server.get('/clientes', (req, res)=>{
     res.send("Pagina de clientes")
 })*/
 
-server.listen(3000, ()=>{
+server.listen(3002, ()=>{
     console.log(`servidor 3002 encendido`)
 })
 
---
+++ appsnodejs/portafolio/routes/route.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/index', (req, res)=>{
+    /*res.send("hola brandon escudero")
+    console.log(__dirname)*/
+    res.render('index')
+})
+
+/*
+router.get('/informacion', (req, res)=>{
+    /*res.send("hola brandon escudero")
+    console.log(__dirname)
+    res.render('informacion')
+})
+
+router.get('/resumen', (req, res)=>{
+    /*res.send("hola brandon escudero")
+    console.log(__dirname)
+    res.render('resumen')
+*/
+
+module.exports = router;
